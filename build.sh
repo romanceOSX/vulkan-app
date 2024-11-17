fd47a29bee4a19fd0642e16ecb9565cc093731df
jeans <catbjeans@gmail.com>
2024-11-17 10:25:36 -0600

    build: added build bash file

---
+++ build.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+# Update app name from cmake to be consumed from this var
+APP_NAME=learning_vulkan
+SRC_DIR=$(dirname "$(realpath "$0")")
+BUILD_DIR=$SRC_DIR/build
+TARGET=$BUILD_DIR/$APP_NAME
+
+if ! [ -d "$BUILD_DIR" ]; then
+    mkdir build
+    cmake -B build
+fi
+
+cmake --build build && $TARGET
+
