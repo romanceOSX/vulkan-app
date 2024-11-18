2fce814f386c83b6270f7f7b38b021beb3fa7e12
jeans <catbjeans@gmail.com>
2024-11-17 22:45:49 -0600

    refactor: moved instance methods to app

--- src/instance.cpp
+++ src/instance.cpp
@@ -7,65 +7,7 @@
 #include "vulkan/vulkan.h"
 #include <vulkan/vulkan_core.h>
 
-VkResult Instance::getInstanceLayerProperties() {
-    VkResult result;
-    uint32_t instanceLayerCount; 
-    std::vector<VkLayerProperties> _layers;
-
-    /* get version */
-    vkEnumerateInstanceVersion(&m_api_version);
-
-    /* get instance properties */
-    vkEnumerateInstanceLayerProperties(&instanceLayerCount, NULL);
-    _layers.resize(instanceLayerCount);
-    m_layers.resize(instanceLayerCount);
-    vkEnumerateInstanceLayerProperties(&instanceLayerCount, _layers.data());
-
-    /* query instance layers properties and save to custom structure */
-    size_t i = 0;
-    for (auto iter = _layers.begin(); iter != _layers.end(); iter++) {
-        m_layers.at(i).properties = *iter;
-        i++;
-    }
-
-    /* populate each layer's extensions if any */
-    for (auto& layer: m_layers) {
-        uint32_t propertyCount;
-        vkEnumerateInstanceExtensionProperties(layer.properties.layerName, &propertyCount, nullptr);
-        layer.extensions.resize(propertyCount);
-        vkEnumerateInstanceExtensionProperties(layer.properties.layerName, &propertyCount, layer.extensions.data());
-    }
-
-    /* populate instance extensions (non-layer) */
-    uint32_t instance_ext_count;
-    vkEnumerateInstanceExtensionProperties(nullptr, &instance_ext_count, nullptr);
-    m_instance_extensions.resize(instance_ext_count);
-    vkEnumerateInstanceExtensionProperties(nullptr, &instance_ext_count, m_instance_extensions.data());
-
-    return result;
-}
-
-VkResult Instance::print_info() {
-    PRETTY_PRINT("Vulkan API Version");
-    std::cout << "v" << getInstanceVersion() << std::endl;
-
-    PRETTY_PRINT("Instance Layers and extensions");
-    for (auto layer: m_layers) {
-        std::cout << "+ " << layer.properties.layerName << std::endl;
-        for (auto extension: layer.extensions) {
-            std::cout << "|--> " << extension.extensionName 
-                << " (v" << extension.specVersion<< ")" << std::endl;
-        }
-    }
-    PRETTY_PRINT("Instance Extensions");
-    for (auto extension: m_instance_extensions) {
-        std::cout << "+ " << extension.extensionName << " (v" <<
-            extension.specVersion << ")" << std::endl;
-    }
-}
-
-uint32_t Instance::getInstanceVersion() {
-    return this->m_api_version;
+Instance::Instance() {
 }
 
 VkResult Instance::createInstance(char_v& layers, char_v& extensions) {
--- src/instance.h
+++ src/instance.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <string>
+#include <vector>
+
+#include "vulkan/vulkan_core.h"
 #include "vulkan/vulkan.h"
-#include "vector"
-#include <vulkan/vulkan_core.h>
+
+/*
+ * Forward Declarations
+ */
+enum class AppResult;
 
 struct LayerProperties {
     VkLayerProperties                       properties;
@@ -13,17 +19,18 @@ struct LayerProperties {
 class Instance {
     using char_v = std::vector<const char*>;
     public:
-        Instance() = default;
+        Instance();
         VkResult init(void);
         VkInstance getInstance(void);
         VkResult createInstance(char_v& layers, char_v& extensions);
         Instance(Instance& other) = delete;
-        VkResult getInstanceLayerProperties();
-        uint32_t getInstanceVersion();
+        AppResult addExtension(const char* ext);
+        AppResult addLayer(const char* layer);
+
         std::vector<LayerProperties> m_layers;
         std::vector<VkExtensionProperties> m_instance_extensions;
-        VkResult print_info();
         VkInstance m_instance;
+
     private:
         uint32_t m_api_version;
 };
