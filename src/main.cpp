a4f23e03d866e0d4a3aa932194059047542fff79
jeans <catbjeans@gmail.com>
2024-11-18 15:54:30 -0600

    refactor: moving calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <memory> 
+#include <stdexcept>
+
 #include <vulkan/vulkan.h>
 #include <vulkan/vulkan_core.h>
 
@@ -7,20 +11,28 @@
 
 int main (int argc, char *argv[]) {
     App* app = App::getInstance();
-    app->printHostInfo();
-    //Instance* vi = app->getVkInstance();
-    //vi->addExtension(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
-    //vi->addExtension(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
-    //vi->addExtension(VK_KHR_SURFACE_EXTENSION_NAME);
-    //vi->addLayer("VK_LAYER_LUNARG_api_dump");
-    //vi->init();
+    Instance* vi = app->getVkInstance();
 
+    /* Extensions needed for MacOS */
+    vi->addExtension(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
+    vi->addExtension(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+    /* Extra Extensions */
+    vi->addExtension(VK_KHR_SURFACE_EXTENSION_NAME);
+    //vi->addLayer("VK_LAYER_LUNARG_api_dump");
+
+    if (VK_SUCCESS != vi->init()) {
+        throw std::runtime_error("Instance creation Failed");
+    }
+
+    app->printHostInfo();
+    
     ///* Print host information */
     //app->getHostInfo();
     
     //auto dev = app->getDefaultPhysicalDevice();
 
     //Device dev(instance);
+    delete app;
     return 0;
 }
 
