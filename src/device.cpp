93dba0a2e7b373978cf3c9d046a79848cccf7eff
jeans <catbjeans@gmail.com>
2024-11-20 22:57:40 -0600

    refactor: removed device host's functions

--- src/device.cpp
+++ src/device.cpp
@@ -20,9 +20,6 @@ void Device::getDeviceQueueProperties(VkPhysicalDevice& dev) {
             << "Queue Flags: " << prop.queueFlags << std::endl;
     }
 
-    /* Physical Groups */
-    getPhysicalDeviceGroups();
-
     /* queue Creation */
     VkDeviceQueueCreateInfo queue_create_info {
         .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
@@ -43,20 +40,6 @@ void Device::getDeviceQueueProperties(VkPhysicalDevice& dev) {
         DBG_ERR("NOT SUCCESS!!");
     }
     PRETTY_PRINT("ALL GOOD");
-}
-
-void Device::getPhysicalDeviceGroups() {
-    uint32_t devGroupsCount;
-    std::vector<VkPhysicalDeviceGroupProperties> devGroups;
-
-    vkEnumeratePhysicalDeviceGroups(m_instance, &devGroupsCount, devGroups.data());
-    devGroups.resize(devGroupsCount);
-    vkEnumeratePhysicalDeviceGroups(m_instance, &devGroupsCount, devGroups.data());
-    
-    PRETTY_PRINT("Device Groups");
-    for (auto& devGroup: devGroups) {
-        std::cout << "Device Count: " << devGroup.physicalDeviceCount << std::endl;
-    }
 }
 
 uint32_t Device::_get_default_index(void) {
