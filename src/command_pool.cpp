fdde97cf16122a41d7ddcf38f8be8947ff85c4ed
jeans <catbjeans@gmail.com>
2025-03-04 22:45:27 -0600

    feat: added command buffer class

--- src/command_pool.cpp
+++ src/command_pool.cpp
@@ -4,6 +4,9 @@
 #include "device.h"
 #include <vulkan/vulkan_core.h>
 
+/*
+ * Command Pool class
+ */
 CommandPool::CommandPool(Device& dev): _m_dev{dev} {
     VkCommandPool commandPool;
     VkCommandPoolCreateInfo command_pool_create = {
@@ -35,16 +38,78 @@ void CommandPool::destroy() {
     );
 }
 
-void CommandPool::_allocate_cmd_buffer() {
-    /* Allocate command buffer from cmd buff pool */
-    VkCommandBuffer cmdBuff;
+VkCommandPool CommandPool::getVkCmdPool() {
+    return _m_command_pool;
+}
 
-    VkCommandBufferAllocateInfo allocate_info;
+Device& CommandPool::getDevice() {
+    return _m_dev;
+}
+
+/*
+ * TODO: Find a better way to pass the Device around 
+ *  all classes in some way related to device, should have a reference to it
+ *  as a member
+ */
+
+/*
+ * Command Buffer class
+ */
+CommandBuffer::CommandBuffer(CommandPool& cmdPool, uint32_t count, Type type)
+    :_m_cmdPool{cmdPool}, _m_count{count}, _m_dev{cmdPool.getDevice()} {
+    /* TODO: This vector could be initialized at the initializer list instead */
+    _m_cmdBuffs.resize(_m_count);
+    
+    VkCommandBufferAllocateInfo allocate_info = {
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+        .pNext = nullptr,
+        .commandPool = _m_cmdPool.getVkCmdPool(),
+        .level = type,
+        .commandBufferCount = _m_count,
+    };
 
     vkAllocateCommandBuffers(
             _m_dev.getVkDevice(),
             &allocate_info,
-            &cmdBuff
+            _m_cmdBuffs.data()
     );  
+}
+
+/* Resets all, edit class upon solid use-case */
+void CommandBuffer::reset() {
+    for (auto& buf: _m_cmdBuffs) {
+        vkResetCommandBuffer(buf, VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT);
+    }
+}
+
+std::vector<VkCommandBuffer>& CommandBuffer::getCmdBuffs() {
+    return _m_cmdBuffs;
+}
+
+void CommandBuffer::free() {
+    vkFreeCommandBuffers(_m_dev.getVkDevice(),
+            _m_cmdPool.getVkCmdPool(),
+            _m_cmdBuffs.size(),
+            _m_cmdBuffs.data()
+    );
+}
+
+void CommandBuffer::begin(uint32_t buf, Usage usage) {
+    VkCommandBufferBeginInfo beginInfo = {
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+        .pNext = nullptr,
+        .flags = usage,
+        .pInheritanceInfo = nullptr,
+    };
+    vkBeginCommandBuffer(_m_cmdBuffs.at(buf), &beginInfo);
+}
+
+#include <tuple>
+/*
+ * Command Buffer submission
+ */
+void CommandBuffer::submit() {
+    auto tup = std::make_tuple(1, "hello");
+    auto [a, b] = tup;
 }
 
--- src/command_pool.h
+++ src/command_pool.h
@@ -15,11 +15,34 @@ class CommandPool {
         void trim();
         void reset();
         void destroy();
+        VkCommandPool getVkCmdPool();
+        Device& getDevice();
 
     private:
         void _allocate_cmd_buffer();
 
         Device&                 _m_dev;
         VkCommandPool           _m_command_pool;
+};
+
+/* TODO: Rename this to CommandBuffers plural! */
+class CommandBuffer {
+    public:
+        using Type = VkCommandBufferLevel;
+        using Usage = VkCommandBufferUsageFlags;
+
+        CommandBuffer(CommandPool& cmdPool, uint32_t count, Type type);
+        void reset();
+        void free();
+        uint32_t count();
+        std::vector<VkCommandBuffer>& getCmdBuffs();
+        void begin(uint32_t buf, Usage usage);
+        void submit();
+
+    private:
+        Device&                         _m_dev;
+        CommandPool&                    _m_cmdPool;
+        std::vector<VkCommandBuffer>    _m_cmdBuffs;
+        uint32_t                        _m_count;
 };
 
