e83891d01c48011b45ad159856a2dfb947c21fec
jeans <catbjeans@gmail.com>
2025-03-17 12:26:54 -0600

    chore: added templated debug functions

--- src/app_settings.hpp
+++ src/app_settings.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 #define APP_WINDOW_NAME     "Vulkan"
 #define APP_WINDOW_WIDTH    800
 #define APP_WINDOW_HEIGHT   600
@@ -16,4 +18,21 @@
 #define APP_PRETTY_PRINT_CUSTOM(msg, symbol) std::cout << #symbol " ~~~ " << msg << " ~~~ " #symbol << std::endl
 #define CHECK() (std::cout << "✅ ✅ ✅" << std::endl)
 #define APP_CHECK() (std::cout << "✅ ✅ ✅" << std::endl)
+
+template <typename T, typename... Args>
+void app_dbg_print(T arg, Args... args) {
+    if constexpr(sizeof...(args) != 0) {
+        std::cout << arg << std::endl;
+        app_dbg_print(args...);
+    }
+}
+
+template <typename T>
+void app_dbg_print_container(T& cont) {
+    std::cout << "🎤 Printing container..." << std::endl;
+    for (auto& item: cont) {
+        std::cout << item << std::endl;
+    }
+    std::cout << "🎤 Finished printing container" << std::endl;
+}
 
