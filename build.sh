722b9eb56084a5c6f0b41519f624598e5339b27c
jeans <catbjeans@gmail.com>
2025-03-18 14:56:43 -0600

    build: consume environment variables from SDK sript

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 
 # Update app name from cmake to be consumed from this var
 export APP_NAME=vulkan_app
-export VULKAN_SDK=~/VulkanSDK/1.3.283.0/macOS
+
+# Set up environment variables
+. /Users/david/VulkanSDK/1.4.309.0/setup-env.sh
+
+export 
+
 SRC_DIR=$(dirname "$(realpath "$0")")
 BUILD_DIR=$SRC_DIR/build
 TARGET=$BUILD_DIR/$APP_NAME
