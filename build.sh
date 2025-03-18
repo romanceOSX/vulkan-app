25376f8947f3c4d48acd19846cc27cdbe7ab2f64
jeans <catbjeans@gmail.com>
2025-03-17 22:34:56 -0600

    build: using cmake's FindVulkan

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # Update app name from cmake to be consumed from this var
 export APP_NAME=vulkan_app
+export VULKAN_SDK=~/VulkanSDK/1.3.283.0/macOS
 SRC_DIR=$(dirname "$(realpath "$0")")
 BUILD_DIR=$SRC_DIR/build
 TARGET=$BUILD_DIR/$APP_NAME
