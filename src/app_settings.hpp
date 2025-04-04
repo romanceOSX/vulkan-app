cb5e30dc4222e20f5f88871dfdf9d035a3ddcfd2
romance <106858958+romanceOSX@users.noreply.github.com>
2025-04-04 12:13:58 -0600

    chore: disable window resizing
    It seems that there is an step to handle variable sized windows

--- src/app_settings.hpp
+++ src/app_settings.hpp
@@ -5,7 +5,7 @@
 #define APP_WINDOW_NAME     "Vulkan"
 #define APP_WINDOW_WIDTH    800
 #define APP_WINDOW_HEIGHT   600
-#define APP_RESIZABLE_WINDOW
+//#define APP_RESIZABLE_WINDOW
 
 /* 
  * Utility Macros
