2f4c06433e28c38ac69d7c2efd9b37b23652b071
romance <106858958+romanceOSX@users.noreply.github.com>
2025-04-05 14:13:33 -0600

    fix(build): fixed compilation paths

--- shaders/glsl/triangle/compile.sh
+++ shaders/glsl/triangle/compile.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+#! /bin/sh
 
-SCRIPT_CWD=$(dirname "$(realpath "$0")")
+SCRIPT_CWD=
 VULKAN_GLSL_COMPILER="glslc"
 VULKAN_SDK="$HOME/VulkanSDK/1.4.309.0"
 GLSLC_PATH="$VULKAN_SDK/macOS/bin/$VULKAN_GLSL_COMPILER"
 
 SHADER_NAME="triangle"
 
-$GLSLC_PATH $SCRIPT_CWD/$SHADER_NAME.vert -o $SCRIPT_CWD/$SHADER_NAME.vert.spv
-$GLSLC_PATH $SCRIPT_CWD/$SHADER_NAME.frag -o $SCRIPT_CWD/$SHADER_NAME.frag.spv
+$GLSLC_PATH shaders/glsl/triangle/$SHADER_NAME.vert -o shaders/glsl/triangle/$SHADER_NAME.vert.spv
+$GLSLC_PATH shaders/glsl/triangle/$SHADER_NAME.frag -o shaders/glsl/triangle/$SHADER_NAME.frag.spv
 
