094baba2a2e02a6bd6787723d15c9e62a133ebc4
romance <106858958+romanceOSX@users.noreply.github.com>
2025-04-06 00:04:06 -0600

    chore: created dedicated app_utils header file

--- src/app_settings.hpp
+++ src/app_settings.hpp
@@ -1,39 +1,9 @@
 #pragma once
 
-#include <iostream>
-
 #define APP_WINDOW_NAME     "Vulkan"
 #define APP_WINDOW_WIDTH    800
 #define APP_WINDOW_HEIGHT   600
-//#define APP_RESIZABLE_WINDOW
+#define APP_RESIZABLE_WINDOW
 
-/* 
- * Utility Macros
- */
-/* TODO: move these macros to an app utility-specific header */
-#define APP_PRETTY_PRINT(str) std::cout << "🎆 ~~~ " << str << " ~~~ 🎆" << std::endl
-#define DBG_ERR(msg) std::cout << "❌❌ " msg << std::endl
-#define APP_DBG_ERR(msg) std::cout << "❌❌ " msg << std::endl
-#define APP_DBG_WARN(msg) std::cout << "⚠️ " << msg << std::endl
-#define APP_PRETTY_PRINT_CUSTOM(msg, symbol) std::cout << symbol << " ~~~ " << msg << " ~~~ " << symbol << std::endl
-#define CHECK() (std::cout << "✅ ✅ ✅" << std::endl)
-#define APP_CHECK() (std::cout << "✅ ✅ ✅" << std::endl)
-#define APP_PRINT_INFO(msg) std::cout << "ℹ️ " << msg << std::endl
-
-template <typename T, typename... Args>
-void app_dbg_print(T arg, Args... args) {
-    if constexpr(sizeof...(args) != 0) {
-        std::cout << arg << std::endl;
-        app_dbg_print(args...);
-    }
-}
-
-template <typename T>
-void app_dbg_print_container(T& cont) {
-    std::cout << "🎤 Printing container..." << std::endl;
-    for (auto& item: cont) {
-        std::cout << item << std::endl;
-    }
-    std::cout << "🎤 Finished printing container" << std::endl;
-}
+#include "app_utils.hpp"
 
---
+++ src/app_utils.hpp
@@ -0,0 +1,35 @@
+/*
+ * General purpose utility functions/macro etc...
+ */
+
+#include <iostream>
+
+/* TODO: move these macros to an app utility-specific header */
+#define APP_PRETTY_PRINT(str) std::cout << "🎆 ~~~ " << str << " ~~~ 🎆" << std::endl
+#define DBG_ERR(msg) std::cout << "❌❌ " msg << std::endl
+#define APP_DBG_ERR(msg) std::cout << "❌❌ " msg << std::endl
+#define APP_DBG_WARN(msg) std::cout << "⚠️ " << msg << std::endl
+#define APP_PRETTY_PRINT_CUSTOM(msg, symbol) std::cout << symbol << " ~~~ " << msg << " ~~~ " << symbol << std::endl
+#define APP_PRETTY_PRINT_CREATE(msg) APP_PRETTY_PRINT_CUSTOM(msg, "☀️")
+#define APP_PRETTY_PRINT_DESTROY(msg) APP_PRETTY_PRINT_CUSTOM(msg, "🌙")
+#define CHECK() (std::cout << "✅ ✅ ✅" << std::endl)
+#define APP_CHECK() (std::cout << "✅ ✅ ✅" << std::endl)
+#define APP_PRINT_INFO(msg) std::cout << "ℹ️ " << msg << std::endl
+
+template <typename T, typename... Args>
+void app_dbg_print(T arg, Args... args) {
+    if constexpr(sizeof...(args) != 0) {
+        std::cout << arg << std::endl;
+        app_dbg_print(args...);
+    }
+}
+
+template <typename T>
+void app_dbg_print_container(T& cont) {
+    std::cout << "🎤 Printing container..." << std::endl;
+    for (auto& item: cont) {
+        std::cout << item << std::endl;
+    }
+    std::cout << "🎤 Finished printing container" << std::endl;
+}
+
