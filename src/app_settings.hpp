7d523f4d114a5e5272a64d5d42f70a9e0727979c
romance <106858958+romanceOSX@users.noreply.github.com>
2025-04-09 18:56:26 -0600

    chore: max frames in flight macro

--- src/app_settings.hpp
+++ src/app_settings.hpp
@@ -5,5 +5,10 @@
 #define APP_WINDOW_HEIGHT   600
 #define APP_RESIZABLE_WINDOW
 
+/*
+ * Number of frames to be processed concurrently
+ */
+#define MAX_FRAMES_IN_FLIGHT    2
+
 #include "app_utils.hpp"
 
