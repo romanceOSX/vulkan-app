37d308ef1005b5ce857a6235c6751e0a4543d6ec
romance <106858958+romanceOSX@users.noreply.github.com>
2025-04-10 19:36:35 -0600

    feat(vertex): initial functional vertex module

--- src/vertex.cpp
+++ src/vertex.cpp
@@ -5,13 +5,36 @@
 #include "vertex.hpp"
 
 const std::vector<Vertex> vertices = {
+    {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+    {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
 };
 
 /* describes which rate to load data from memory througout the vertices */
 VkVertexInputBindingDescription VertexInput::get_binding_description() {
+    VkVertexInputBindingDescription binding_description{};
+    binding_description.binding = 0;
+    binding_description.stride = sizeof(Vertex);
+    binding_description.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+    return binding_description;
 }
 
 /* position and color vertex attributes */
 std::array<VkVertexInputAttributeDescription, 2> VertexInput::get_attribute_descriptions() {
+    std::array<VkVertexInputAttributeDescription, 2> attribute_descriptions{};
+    /* vertex data */
+    attribute_descriptions[0].binding = 0;
+    attribute_descriptions[0].location = 0;
+    attribute_descriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attribute_descriptions[0].offset = offsetof(Vertex, pos);
+
+    /* fragment data */
+    attribute_descriptions[1].binding = 0;
+    attribute_descriptions[1].location = 1;
+    attribute_descriptions[1].format = VK_FORMAT_R32G32_SFLOAT;
+    attribute_descriptions[1].offset = offsetof(Vertex, color);
+
+    return attribute_descriptions;
 }
 
