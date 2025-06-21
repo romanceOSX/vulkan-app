de58de33f6aceae379199146d080ed30c645d89f
romance <106858958+romanceOSX@users.noreply.github.com>
2025-06-21 15:08:45 -0600

    test: testing current executable directory

--- test/test.cpp
+++ test/test.cpp
@@ -2,12 +2,19 @@
 #include "utils.hpp"
 
 void _test_current_exe_path() {
-    std::cout << "testing current_exe_path" << std::endl;
+    std::cout << "testing utils_get_exe_path" << std::endl;
     std::cout << utils_get_exe_path() << std::endl;
 }
 
+void _test_current_exe_dir() {
+    std::cout << "testing utils_get_exe_dir" << std::endl;
+    std::cout << utils_get_exe_dir() << std::endl;
+}
+
+
 int main (int argc, char *argv[]) {
     _test_current_exe_path();
+    _test_current_exe_dir();
     return 0;
 }
 
