6c6a737c892ea7e911f332a2f1a161e3bf24c4a4
romance <106858958+romanceOSX@users.noreply.github.com>
2025-07-07 20:06:19 -0600

    feat: usable buffer class

--- app/buffer.cpp
+++ app/buffer.cpp
@@ -1,9 +1,11 @@
 #include <vulkan/vulkan.h>
 
 #include <optional>
+#include <vulkan/vulkan_core.h>
 
 #include "app_settings.hpp"
 #include "buffer.hpp"
+#include "command_pool.hpp"
 #include "device.hpp"
 #include "physical_device.hpp"
 
@@ -65,7 +67,61 @@ void Buffer::unmap_memory() {
     vkUnmapMemory(m_dev, m_vk_buffer_memory);
 }
 
-VkBuffer Buffer::get_vk_buffer() {
+VkBuffer Buffer::get_vk_buf() {
     return m_vk_buffer;
+}
+
+Device& Buffer::get_dev() {
+    return m_dev;
+}
+
+void Buffer::copy_buffer(
+    CommandPool& cmd_pool,
+    Buffer &src,
+    Buffer &dst,
+    VkDeviceSize size
+) {
+    auto& dev = src.get_dev();
+
+    /* create temporary command buffer to command memory transfer operation */
+    VkCommandBufferAllocateInfo alloc_info{};
+    alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    alloc_info.commandPool = cmd_pool.get_vk_command_pool();
+    alloc_info.commandBufferCount = 1;
+
+    VkCommandBuffer cmd_buf;
+    vkAllocateCommandBuffers(dev.get_vk_device(), &alloc_info, &cmd_buf);
+
+    /* record operation */
+    VkCommandBufferBeginInfo begin_info{};
+    begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+    vkBeginCommandBuffer(cmd_buf, &begin_info);
+
+    /* copy the memory from src to dst */
+    VkBufferCopy copy_region{};
+    copy_region.srcOffset = 0;
+    copy_region.dstOffset = 0;
+    copy_region.size = size;
+    vkCmdCopyBuffer(cmd_buf, src.get_vk_buf(), dst.get_vk_buf(), 1, &copy_region);
+
+    vkEndCommandBuffer(cmd_buf);
+
+    /* submit the command */
+    VkSubmitInfo submit_info{};
+    submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submit_info.commandBufferCount = 1;
+    submit_info.pCommandBuffers = &cmd_buf;
+
+    /* get queue out of device */ 
+    VkQueue queue = cmd_pool.get_device().get_vk_queue(cmd_pool.get_queue_family());
+
+    /* we could wait through a fence but it is not necesary */
+    vkQueueSubmit(queue, 1, &submit_info, VK_NULL_HANDLE);
+    vkQueueWaitIdle(queue);
+
+    vkFreeCommandBuffers(dev.get_vk_device(), cmd_pool.get_vk_command_pool(), 1, &cmd_buf);
 }
 
--- app/buffer.hpp
+++ app/buffer.hpp
@@ -3,11 +3,15 @@
 #include <vulkan/vulkan.h>
 
 class Device;
+class CommandPool;
 
 class Buffer {
     public:
+        static void copy_buffer(CommandPool& cmd_pool, Buffer& src, Buffer& dst, VkDeviceSize size);
+
         Buffer(Device& dev, VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties);
-        VkBuffer get_vk_buffer();
+        VkBuffer get_vk_buf();
+        Device& get_dev();
         void map_memory(void** data);
         void unmap_memory();
 
