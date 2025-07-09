fa682185bd4661b2271e6525fba7d7b02e19c861
romance <106858958+romanceOSX@users.noreply.github.com>
2025-07-08 22:35:37 -0600

    test: included new test for vulkanhpp

---
+++ test/app-vulkan-hpp/main.cpp
@@ -0,0 +1,53 @@
+#define VULKAN_HPP_NO_CONSTRUCTORS
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_raii.hpp>
+
+#include <iostream>
+
+static std::string AppName    = "01_InitInstanceRAII";
+static std::string EngineName = "Vulkan.hpp";
+
+int main( int /*argc*/, char ** /*argv*/ )
+{
+  /* VULKAN_HPP_KEY_START */
+
+  try
+  {
+    // the very beginning: instantiate a context
+    /* The vulkan context is in charge of acting as a dispatcher for the vulkan loader */
+    vk::raii::Context context;
+
+    // initialize the vk::ApplicationInfo structure
+    vk::ApplicationInfo applicationInfo{
+            .pApplicationName = AppName.c_str(),  
+        };
+    //vk::ApplicationInfo applicationInfo( AppName.c_str(), 1, EngineName.c_str(), 1, VK_API_VERSION_1_1 );
+
+    // initialize the vk::InstanceCreateInfo
+    vk::InstanceCreateInfo instanceCreateInfo( {}, &applicationInfo );
+
+    // create an Instance
+    vk::raii::Instance instance( context, instanceCreateInfo );
+  }
+  catch ( vk::SystemError & err )
+  {
+    std::cout << "vk::SystemError: " << err.what() << std::endl;
+    exit( -1 );
+  }
+  catch ( std::exception & err )
+  {
+    std::cout << "std::exception: " << err.what() << std::endl;
+    exit( -1 );
+  }
+  catch ( ... )
+  {
+    std::cout << "unknown error\n";
+    exit( -1 );
+  }
+
+  /* VULKAN_HPP_KEY_END */
+
+  return 0;
+}
+
