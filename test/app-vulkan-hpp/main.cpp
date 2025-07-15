63e880f2512e3092f0f413032adab7114fef7938
romance <106858958+romanceOSX@users.noreply.github.com>
2025-07-14 21:14:15 -0600

    chore: formatting change

--- test/app-vulkan-hpp/main.cpp
+++ test/app-vulkan-hpp/main.cpp
@@ -10,46 +10,51 @@ static std::string EngineName = "Vulkan.hpp";
 
 int main( int /*argc*/, char ** /*argv*/ )
 {
-  /* VULKAN_HPP_KEY_START */
+    /* VULKAN_HPP_KEY_START */
 
-  try
-  {
-    // the very beginning: instantiate a context
-    /* The vulkan context is in charge of acting as a dispatcher for the vulkan loader */
-    vk::raii::Context context;
+    try
+    {
+        // the very beginning: instantiate a context
+        /* The vulkan context is in charge of acting as a dispatcher for the vulkan loader */
+        /* 
+         * The dispatcher is in charge of getting the correct function to call out of the
+         * vulkan loader at runtime
+         *  --> https://github.com/KhronosGroup/Vulkan-Loader
+         */
+        vk::raii::Context context;
 
-    // initialize the vk::ApplicationInfo structure
-    vk::ApplicationInfo applicationInfo{
+        // initialize the vk::ApplicationInfo structure
+        vk::ApplicationInfo app_info{
             .pApplicationName = AppName.c_str(),  
         };
 
-    vk::ApplicationInfo applicationInfo( AppName.c_str(), 1, EngineName.c_str(), 1, VK_API_VERSION_1_1 );
+        //vk::ApplicationInfo applicationInfo( AppName.c_str(), 1, EngineName.c_str(), 1, VK_API_VERSION_1_1 );
 
-    // initialize the vk::InstanceCreateInfo
+        // initialize the vk::InstanceCreateInfo
 
-    vk::InstanceCreateInfo instanceCreateInfo( {}, &applicationInfo );
+        vk::InstanceCreateInfo instanceCreateInfo( {}, &app_info );
 
-    // create an Instance
-    vk::raii::Instance instance( context, instanceCreateInfo );
-  }
-  catch ( vk::SystemError & err )
-  {
-    std::cout << "vk::SystemError: " << err.what() << std::endl;
-    exit( -1 );
-  }
-  catch ( std::exception & err )
-  {
-    std::cout << "std::exception: " << err.what() << std::endl;
-    exit( -1 );
-  }
-  catch ( ... )
-  {
-    std::cout << "unknown error\n";
-    exit( -1 );
-  }
+        // create an Instance
+        vk::raii::Instance instance( context, instanceCreateInfo );
+    }
+    catch ( vk::SystemError & err )
+    {
+        std::cout << "vk::SystemError: " << err.what() << std::endl;
+        exit( -1 );
+    }
+    catch ( std::exception & err )
+    {
+        std::cout << "std::exception: " << err.what() << std::endl;
+        exit( -1 );
+    }
+    catch ( ... )
+    {
+        std::cout << "unknown error\n";
+        exit( -1 );
+    }
 
-  /* VULKAN_HPP_KEY_END */
+    /* VULKAN_HPP_KEY_END */
 
-  return 0;
+    return 0;
 }
 
