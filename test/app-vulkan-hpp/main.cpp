1d055d4e2439776dee78aa4bdbaf3ba7ae57c50d
romance <106858958+romanceOSX@users.noreply.github.com>
2025-08-22 21:22:52 -0600

    chore: overriding ostreams

--- test/app-vulkan-hpp/main.cpp
+++ test/app-vulkan-hpp/main.cpp
@@ -1,41 +1,89 @@
+/*
+ * Vulkan-hpp sample test
+ */
+
+#include <iostream>
+#include <memory>
+#include <print>
+#include <sstream>
+#include <vector>
+
 #define VULKAN_HPP_NO_CONSTRUCTORS
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vulkan_raii.hpp>
 
-#include <iostream>
-
 static std::string AppName    = "01_InitInstanceRAII";
 static std::string EngineName = "Vulkan.hpp";
 
-int main( int /*argc*/, char ** /*argv*/ )
-{
-    /* VULKAN_HPP_KEY_START */
+using std::vector;
 
+void print_available_instance_layers(vk::raii::Context &ctx) {
+    vector<vk::LayerProperties> properties = ctx.enumerateInstanceLayerProperties();
+    for (auto& p: properties) {
+        std::cout 
+            << p.layerName << std::endl
+            << p.description << std::endl;
+    }
+}
+
+std::ostream& operator<<(std::ostream& stream, vector<vk::LayerProperties> layers) {
+    for (auto& layer: layers) {
+        stream
+            << "\t- "
+            << layer.layerName << " (" << layer.description << ")" << std::endl
+        ;
+    }
+
+    return stream;
+}
+
+void scan_system(vk::raii::Context &ctx) {
+    std::cout << ctx.enumerateInstanceLayerProperties();
+
+    //vector<vk::ExtensionProperties> extensions = ctx.enumerateInstanceExtensionProperties();
+    //for (auto& ext: extensions) {
+    //    std::cout
+    //        << "\t- "
+    //        << ext.extensionName << std::endl
+    //        ;
+    //}
+}
+
+int test_template() {
+    /* VULKAN_HPP_KEY_START */
     try
     {
-        // the very beginning: instantiate a context
-        /* The vulkan context is in charge of acting as a dispatcher for the vulkan loader */
         /* 
+         * The vulkan context is in charge of acting as a dispatcher for the vulkan loader
+         *
          * The dispatcher is in charge of getting the correct function to call out of the
          * vulkan loader at runtime
          *  --> https://github.com/KhronosGroup/Vulkan-Loader
          */
         vk::raii::Context context;
 
-        // initialize the vk::ApplicationInfo structure
+        scan_system(context);
+
+        /* get instance layer properties */
+        std::vector<vk::LayerProperties> layers = context.enumerateInstanceLayerProperties();
+
+        for (auto& layer: layers) {
+            std::cout << layer.layerName << "\t" << layer.description << std::endl;
+        }
+
+        /* fill-in app info */
         vk::ApplicationInfo app_info{
             .pApplicationName = AppName.c_str(),  
         };
 
-        //vk::ApplicationInfo applicationInfo( AppName.c_str(), 1, EngineName.c_str(), 1, VK_API_VERSION_1_1 );
+        //vk::InstanceCreateInfo instance_create_info({}, &app_info);
+        vk::InstanceCreateInfo instance_create_info{
+            .pApplicationInfo = &app_info,
+        };
 
-        // initialize the vk::InstanceCreateInfo
-
-        vk::InstanceCreateInfo instanceCreateInfo( {}, &app_info );
-
-        // create an Instance
-        vk::raii::Instance instance( context, instanceCreateInfo );
+        /* create instance */
+        auto instance = std::make_unique<vk::raii::Instance>(context, instance_create_info);
     }
     catch ( vk::SystemError & err )
     {
@@ -56,5 +104,16 @@ int main( int /*argc*/, char ** /*argv*/ )
     /* VULKAN_HPP_KEY_END */
 
     return 0;
+}
+
+void test_vulkan() {
+    vk::raii::Context ctx;
+    scan_system(ctx);
+}
+
+int main( int /*argc*/, char ** /*argv*/ )
+{
+    test_vulkan();
+    //test_template();
 }
 
