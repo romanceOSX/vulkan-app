f7e0b317645aa566965a5146d63bacbfe3830ccf
romance <106858958+romanceOSX@users.noreply.github.com>
2025-09-15 00:10:52 -0600

    feat: utilshpp

---
+++ test/app-vulkan-hpp/utils/utils.cpp
@@ -0,0 +1,10 @@
+#include "utils.hpp"
+namespace vu {
+
+// from a phy_dev and a surface find the 'presentation' queue family
+// prioritize being the graphic bit first
+
+// get graphic bit from a device
+
+}
+
